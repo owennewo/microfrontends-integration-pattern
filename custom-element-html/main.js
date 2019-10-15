d04861ee3b2187ab727380390ae59ace2bbd89a4
Owen Williams <williams.owen@gmail.com>
2019-10-15 10:40:07 +0100

    moving hosts to localhost

--- custom-element-html/main.js
+++ custom-element-html/main.js
@@ -1,54 +1,53 @@
-var templateHTML = `
-<p>html element says: {{text}} </p>
-<button type="submit" class="btn btn-secondary">Send text</button>`;
+var templateHTML = '<p>html element says: {{text}} </p><button type="submit" class="btn btn-secondary">Send text</button>';
 
 class ChildHtmlElement extends HTMLElement {
     constructor() {
-      super();
+        super();
 
-      this._shadowRoot = this.attachShadow({ mode: 'open' });
-      this.replaceContent(this.getAttribute('text'));
-      
+        this._shadowRoot = this.attachShadow({ mode: 'open' });
+        this.replaceContent(this.getAttribute('text'));
+
     }
 
-    
+
 
     static get observedAttributes() {
         return ['text'];
-      }
-      
-      attributeChangedCallback(name, oldValue, newValue) {
-        switch (name) {
-          case 'text':
-            console.log('attribute has change to \'' + newValue +'\'');
-            this.replaceContent(newValue);
-            break;
-         default:
-             console.log('unknown attribute: ' + name);
-        }
-      }
+    }
 
-      replaceContent(text) {
+    attributeChangedCallback(name, oldValue, newValue) {
+        switch (name) {
+            case 'text':
+                console.log('attribute has change to \'' + newValue + '\'');
+                this.replaceContent(newValue);
+                break;
+            default:
+                console.log('unknown attribute: ' + name);
+        }
+    }
+
+    replaceContent(text) {
         while (this._shadowRoot.firstChild) this._shadowRoot.firstChild.remove();
-      
+
         const template = document.createElement('template');
         template.innerHTML = templateHTML.replace('{{text}}', text);
-        
+
         var node = template.content.cloneNode(true);
 
         node.lastChild.addEventListener('click', this.onClick);
         this._shadowRoot.appendChild(node);
-      }
+    }
 
-      onClick() {
+    onClick() {
         this.dispatchEvent(new CustomEvent('onHelloEvt',
-        {
-            bubbles: true,
-            cancelable: false,
-            composed: true,
-            detail: 'sending message'}
+            {
+                bubbles: true,
+                cancelable: false,
+                composed: true,
+                detail: 'sending message'
+            }
         ));
-      }
-  }
+    }
+}
 
-  window.customElements.define('custom-element-html', ChildHtmlElement);
+window.customElements.define('custom-element-html', ChildHtmlElement);
